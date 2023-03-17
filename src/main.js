f61a0bf18c6e7ce9858e5cdb5dcf21554e94058b
Jakob Tarp <jakob.tarp@outlook.com>
2023-03-18 00:18:43 +0100

    burgermenu_manglerhidden

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import "./input.css";
 import "./style.css";
 
+import Alpine from "alpinejs";
+window.Alpine = Alpine;
+Alpine.start();
+
 /* Import af Motion One bibliotek */
 import { animate, stagger, inView, scroll, timeline } from "motion";
 
