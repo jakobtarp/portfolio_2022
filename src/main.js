672cd92da6b4f9cdfd5be22d32f0dca3d6bf1b08
Jakob Tarp <jakob.tarp@outlook.com>
2023-03-18 14:43:54 +0100

    burgermenu

--- src/main.js
+++ src/main.js
@@ -70,24 +70,24 @@ inView(".box7", () => {
 
 scroll(animate(".progress", { strokeDasharray: ["0,1", "1,1"] }));
 
-const navSlide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links a");
+// const navSlide = () => {
+//   const burger = document.querySelector(".burger");
+//   const nav = document.querySelector(".nav-links");
+//   const navLinks = document.querySelectorAll(".nav-links a");
 
-  burger.addEventListener("click", () => {
-    nav.classList.toggle("nav-active");
+//   burger.addEventListener("click", () => {
+//     nav.classList.toggle("nav-active");
 
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s `;
-      }
-    });
-    burger.classList.toggle("toggle");
-  });
-  //
-};
+//     navLinks.forEach((link, index) => {
+//       if (link.style.animation) {
+//         link.style.animation = "";
+//       } else {
+//         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s `;
+//       }
+//     });
+//     burger.classList.toggle("toggle");
+//   });
+//   //
+// };
 
-navSlide();
+// navSlide();
